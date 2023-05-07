d99279540e70a3d3e97bc3a56fca813043364532
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2023-05-07 10:16:52 -0300

    refactor: use cube class in cube generation

--- terrain/Terrain.cs
+++ terrain/Terrain.cs
@@ -22,106 +22,60 @@ public partial class Terrain : Node3D
 					("the terrain's cube size must be greater than 0");
 			}
 
-			var vertices = new Vector3[]
-			{
-				// Face 0
-				new(0, 0, 0),
-				new(1, 0, 0),
-				new(1, 1, 0),
-				new(0, 1, 0),
-				// Face 1
-				new(1, 0, 0),
-				new(1, 0, 1),
-				new(1, 1, 1),
-				new(1, 1, 0),
-				// Face 2
-				new(1, 0, 1),
-				new(0, 0, 1),
-				new(0, 1, 1),
-				new(1, 1, 1),
-				// Face 3
-				new(0, 0, 1),
-				new(0, 0, 0),
-				new(0, 1, 0),
-				new(0, 1, 1),
-				// Face 4
-				new(0, 0, 1),
-				new(1, 0, 1),
-				new(1, 0, 0),
-				new(0, 0, 0),
-				// Face 5
-				new(0, 1, 0),
-				new(1, 1, 0),
-				new(1, 1, 1),
-				new(0, 1, 1),
-			};
+			var uniqueVertices = new Vector3[Cube.VerticesCount];
+			var uniqueVerticesNormals = new Vector3[Cube.VerticesCount];
+			var verticesIndices = new int[Cube.TotalVerticesCount];
 
-			for (var i = 0; i < vertices.Length; ++i)
+			var uniqueVertexIndex = 0;
+			var vertexIndexIndex = 0;
+
+			for (var sideIndex = 0; sideIndex < Cube.Sides.Length; ++sideIndex)
 			{
-				vertices[i] *= CubeSize;
-				vertices[i] += CubePosition;
+				var side = Cube.Sides[sideIndex];
+
+				for
+				(
+					var sideUniqueVertexIndex = 0;
+					sideUniqueVertexIndex < Cube.UniqueVerticesCountPerSide;
+					++sideUniqueVertexIndex
+				)
+				{
+					var sideUniqueVertex =
+						Cube.SidesUniqueVertices[side][sideUniqueVertexIndex];
+
+					uniqueVertices[uniqueVertexIndex] =
+						sideUniqueVertex * CubeSize + CubePosition;
+					
+					uniqueVerticesNormals[uniqueVertexIndex] =
+						Cube.SidesNormals[side];
+
+					++uniqueVertexIndex;
+				}
+
+				for
+				(
+					var sideVertexIndexIndex = 0;
+					sideVertexIndexIndex < Cube.TotalVerticesCountPerSide;
+					++sideVertexIndexIndex
+				)
+			{
+					var vertexIndexOffset =
+						sideIndex * Cube.UniqueVerticesCountPerSide;
+
+					verticesIndices[vertexIndexIndex] =
+						Cube.SideVerticesIndices[sideVertexIndexIndex]
+						+ vertexIndexOffset;
+					
+					++vertexIndexIndex;
+				}
 			}
 
 			var arrays = new Godot.Collections.Array();
 			arrays.Resize((int)Mesh.ArrayType.Max);
 
-			arrays[(int)Mesh.ArrayType.Vertex] = vertices;
-
-			arrays[(int)Mesh.ArrayType.Index] = new[]
-			{
-				// Face 0
-				0, 1, 3,
-				1, 2, 3,
-				// Face 1
-				4, 5, 7,
-				5, 6, 7,
-				// Face 2
-				8, 9, 11,
-				9, 10, 11,
-				// Face 3
-				12, 13, 15,
-				13, 14, 15,
-				// Face 4
-				16, 17, 19,
-				17, 18, 19,
-				// Face 5
-				20, 21, 23,
-				21, 22, 23,
-			};
-
-			arrays[(int)Mesh.ArrayType.Normal] = new Vector3[]
-			{
-				// Face 0
-				new(0, 0, -1),
-				new(0, 0, -1),
-				new(0, 0, -1),
-				new(0, 0, -1),
-				// Face 1
-				new(1, 0, 0),
-				new(1, 0, 0),
-				new(1, 0, 0),
-				new(1, 0, 0),
-				// Face 2
-				new(0, 0, 1),
-				new(0, 0, 1),
-				new(0, 0, 1),
-				new(0, 0, 1),
-				// Face 3
-				new(-1, 0, 0),
-				new(-1, 0, 0),
-				new(-1, 0, 0),
-				new(-1, 0, 0),
-				// Face 4
-				new(0, -1, 0),
-				new(0, -1, 0),
-				new(0, -1, 0),
-				new(0, -1, 0),
-				// Face 5
-				new(0, 1, 0),
-				new(0, 1, 0),
-				new(0, 1, 0),
-				new(0, 1, 0),
-			};
+			arrays[(int)Mesh.ArrayType.Vertex] = uniqueVertices;
+			arrays[(int)Mesh.ArrayType.Normal] = uniqueVerticesNormals;
+			arrays[(int)Mesh.ArrayType.Index] = verticesIndices;
 
 			var mesh = new ArrayMesh();
 			mesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, arrays);
