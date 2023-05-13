bc80d209a82e7afcc4056c44716f5548c032b109
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2023-05-13 10:30:38 -0300

    feat: make ant spawn on ground

--- world/World.cs
+++ world/World.cs
@@ -10,8 +10,30 @@ public partial class World : Node3D
 	public override void _Ready()
 	{
 		var voxels = GetNode<Voxels.Voxels>("Voxels");
-		var terrain = GetNode<Terrain.Terrain>("Terrain");
 
-		terrain.Generate(voxels);
+		GenerateTerrain();
+		PlaceAntOnGround();
+
+		void GenerateTerrain()
+		{
+			var terrain = GetNode<Terrain.Terrain>("Terrain");
+			terrain.Generate(voxels);
+		}
+
+		void PlaceAntOnGround()
+		{
+			var ant = GetNode<Ant>("Ant");
+			var position = (Vector3I)ant.Position;
+
+			for (position.Y = voxels.Size.Y - 1; position.Y >= 0; --position.Y)
+			{
+				if (voxels[position].Type == Voxels.Type.Dirt)
+				{
+					break;
+				}
+			}
+
+			ant.Position = ant.Position with { Y = position.Y + 1 };
+		}
 	}
 }
