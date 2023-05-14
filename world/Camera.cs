4508c688ad72d4ed9adf5bec036987f103cd1910
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2023-05-14 18:15:11 -0300

    feat: implement camera rotation

--- world/Camera.cs
+++ world/Camera.cs
@@ -3,7 +3,13 @@ using System;
 
 public partial class Camera : Camera3D
 {
+	private const float MinimumXRotation = -90;
+	private const float MaximumXRotation = 90;
+
 	private float _movementSpeed;
+	private float _rotationSpeedInDegrees;
+
+	private Vector2 _mouseMotionDelta;
 
 	/// <summary>
 	/// How fast the camera moves linearly in world units.
@@ -25,41 +31,103 @@ public partial class Camera : Camera3D
 		}
 	}
 
+	/// <summary>
+	/// How fast the camera rotates in degrees.
+	/// </summary>
+	[Export]
+	public float RotationSpeedInDegrees
+	{
+		get => _rotationSpeedInDegrees;
+
+		set
+		{
+			if (value <= 0)
+			{
+				throw new ArgumentOutOfRangeException
+					("the camera rotation speed must be greater than 0");
+			}
+
+			_rotationSpeedInDegrees = value;
+		}
+	}
+
 	public override void _Process(double delta)
 	{
-		var direction = Vector3.Zero;
+		HandleMovement();
+		HandleRotation();
 
-		if (Input.IsActionPressed("move_camera_left"))
+		void HandleMovement()
 		{
-			direction.X += -1;
+			var direction = Vector3.Zero;
+
+			if (Input.IsActionPressed("move_camera_left"))
+			{
+				direction.X += -1;
+			}
+
+			if (Input.IsActionPressed("move_camera_right"))
+			{
+				direction.X += 1;
+			}
+
+			if (Input.IsActionPressed("move_camera_down"))
+			{
+				direction.Y += -1;
+			}
+
+			if (Input.IsActionPressed("move_camera_up"))
+			{
+				direction.Y += 1;
+			}
+
+			if (Input.IsActionPressed("move_camera_forward"))
+			{
+				direction.Z += -1;
+			}
+
+			if (Input.IsActionPressed("move_camera_backward"))
+			{
+				direction.Z += 1;
+			}
+
+			var velocity = direction * MovementSpeed * (float)delta;
+			TranslateObjectLocal(velocity);
+		}
+	
+		void HandleRotation()
+		{
+			_mouseMotionDelta *= RotationSpeedInDegrees * (float)delta;
+
+			RotationDegrees = RotationDegrees with
+			{ 
+				X = Math.Clamp
+				(
+					RotationDegrees.X + _mouseMotionDelta.Y,
+					MinimumXRotation,
+					MaximumXRotation
+				),
+
+				Y = RotationDegrees.Y + _mouseMotionDelta.X
+			};
+
+			_mouseMotionDelta = Vector2.Zero;
+		}
+	}
+
+	public override void _Input(InputEvent inputEvent)
+	{
+		if (inputEvent is not InputEventMouseMotion inputEventMouseMotion)
+		{
+			return;
 		}
 
-		if (Input.IsActionPressed("move_camera_right"))
+		if (!Input.IsActionPressed("rotate_camera"))
 		{
-			direction.X += 1;
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+			return;
 		}
 
-		if (Input.IsActionPressed("move_camera_down"))
-		{
-			direction.Y += -1;
-		}
-
-		if (Input.IsActionPressed("move_camera_up"))
-		{
-			direction.Y += 1;
-		}
-
-		if (Input.IsActionPressed("move_camera_forward"))
-		{
-			direction.Z += -1;
-		}
-
-		if (Input.IsActionPressed("move_camera_backward"))
-		{
-			direction.Z += 1;
-		}
-
-		var velocity = direction * MovementSpeed * (float)delta;
-		TranslateObjectLocal(velocity);
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+		_mouseMotionDelta -= inputEventMouseMotion.Relative;
 	}
 }
